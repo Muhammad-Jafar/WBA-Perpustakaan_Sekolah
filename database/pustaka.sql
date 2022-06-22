737746ce3c2d0e12c571dc1a7b2d5b3974bd9c8b
Jafar 131 <57802779+Muhammad-Jafar@users.noreply.github.com>
2022-06-22 23:39:53 +0800

    Final Update 1.1 ( lupa apdet database )

--- database/pustaka.sql
+++ database/pustaka.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 20, 2022 at 05:47 PM
+-- Generation Time: Jun 22, 2022 at 05:38 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -43,7 +43,8 @@ CREATE TABLE `admin` (
 INSERT INTO `admin` (`id_admin`, `level_user`, `nama_admin`, `username`, `password`, `avatar`) VALUES
 (11, 'admin', 'Harry Potter', 'admin5', '827ccb0eea8a706c4c34a16891f84e7b', '800px-Gado_gado2.jpg'),
 (12, 'admin', 'Novi', 'admin', '827ccb0eea8a706c4c34a16891f84e7b', 'AVATAR_ADMIN-84.png'),
-(13, 'admin', 'Inisialku', 'admin2', '827ccb0eea8a706c4c34a16891f84e7b', 'Pokmon-Detective-Pikachu.jpg');
+(13, 'admin', 'Inisialku', 'admin2', '827ccb0eea8a706c4c34a16891f84e7b', 'Pokmon-Detective-Pikachu.jpg'),
+(14, 'kepsek', 'Kepala Sekolah', 'kepsek', '827ccb0eea8a706c4c34a16891f84e7b', 'AVATAR_ADMIN-85.png');
 
 -- --------------------------------------------------------
 
@@ -95,9 +96,9 @@ CREATE TABLE `buku` (
 --
 
 INSERT INTO `buku` (`id_buku`, `id_jenis_buku`, `kategori_buku`, `kode_buku`, `judul_buku`, `pengarang`, `penerbit`, `tahun_terbit`, `jumlah_halaman`, `qt`) VALUES
-(1, 10, 'Teks-pelajaran', 'BUKU-00002', 'Pengantar Fisika Dasar', 'Robert Dawney Jr', 'Gramedia', '2022', 250, 12),
+(1, 10, 'Teks-pelajaran', 'BUKU-00002', 'Pengantar Fisika Dasar', 'Robert Dawney Jr', 'Gramedia', '2022', 250, 10),
 (2, 9, 'Non Teks-pelajaran', 'BUKU-00003', 'Filsafat Islam', 'Anonymous', 'Gramedia', '2017', 319, 6),
-(3, 10, 'Non Teks-pelajaran', 'BUKU-00004', 'Materi Gelap dan Anti Materi', 'Robert Dawney Jr', 'Gramedia', '2019', 512, 5);
+(3, 10, 'Non Teks-pelajaran', 'BUKU-00004', 'Materi Gelap dan Anti Materi', 'Robert Dawney Jr', 'Gramedia', '2019', 512, 4);
 
 -- --------------------------------------------------------
 
@@ -114,6 +115,18 @@ CREATE TABLE `denda` (
   `telat` int(3) NOT NULL,
   `denda` int(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `denda`
+--
+
+INSERT INTO `denda` (`id_transaksi`, `id_buku`, `id_anggota`, `tgl_kembali`, `tgl_dikembalikan`, `telat`, `denda`) VALUES
+(1, 3, 1, '2022-06-26', '2022-06-21', 0, 0),
+(2, 1, 4, '2022-06-26', '2022-06-21', 0, 0),
+(3, 3, 2, '2022-06-26', '0000-00-00', 0, 0),
+(4, 1, 3, '2022-06-15', '2022-06-21', 6, 3000),
+(5, 1, 1, '2022-06-26', '0000-00-00', 0, 0),
+(6, 1, 1, '2022-06-10', '0000-00-00', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -145,6 +158,28 @@ INSERT INTO `jenis_buku` (`id_jenis_buku`, `jenis_buku`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `sk_bp`
+--
+
+CREATE TABLE `sk_bp` (
+  `id_laporan` int(3) NOT NULL,
+  `kode_sk` text NOT NULL,
+  `perihal` text NOT NULL DEFAULT 'SK Bebas Pustaka',
+  `id_anggota` int(3) NOT NULL,
+  `tgl_terbit` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sk_bp`
+--
+
+INSERT INTO `sk_bp` (`id_laporan`, `kode_sk`, `perihal`, `id_anggota`, `tgl_terbit`) VALUES
+(1, 'SK/Bebas Pustaka/2022/0001', 'SK Bebas Pustaka', 1, '2022-06-22'),
+(2, 'SK/Bebas Pustaka/2022/0002', 'SK Bebas Pustaka', 4, '2022-06-22');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `transaksi`
 --
 
@@ -159,6 +194,18 @@ CREATE TABLE `transaksi` (
   `status` enum('dipinjam','dikembalikan') NOT NULL,
   `qt_pinjam` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `kode_pinjam`, `id_buku`, `id_anggota`, `tgl_pinjam`, `tgl_kembali`, `tgl_dikembalikan`, `status`, `qt_pinjam`) VALUES
+(1, 'PJ-0001', 3, 1, '2022-06-21', '2022-06-26', '2022-06-21', 'dikembalikan', 0),
+(2, 'PJ-0002', 1, 4, '2022-06-21', '2022-06-26', '2022-06-21', 'dikembalikan', 0),
+(3, 'PJ-0003', 3, 2, '2022-06-21', '2022-06-26', '0000-00-00', 'dipinjam', 1),
+(4, 'PJ-0004', 1, 3, '2022-06-10', '2022-06-15', '2022-06-21', 'dikembalikan', 0),
+(5, 'PJ-0005', 1, 1, '2022-06-21', '2022-06-26', '0000-00-00', 'dipinjam', 1),
+(6, 'PJ-0006', 1, 1, '2022-06-05', '2022-06-10', '0000-00-00', 'dipinjam', 1);
 
 --
 -- Triggers `transaksi`
@@ -240,6 +287,12 @@ ALTER TABLE `jenis_buku`
   ADD PRIMARY KEY (`id_jenis_buku`);
 
 --
+-- Indexes for table `sk_bp`
+--
+ALTER TABLE `sk_bp`
+  ADD PRIMARY KEY (`id_laporan`);
+
+--
 -- Indexes for table `transaksi`
 --
 ALTER TABLE `transaksi`
@@ -253,7 +306,7 @@ ALTER TABLE `transaksi`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id_admin` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_admin` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `anggota`
@@ -271,7 +324,7 @@ ALTER TABLE `buku`
 -- AUTO_INCREMENT for table `denda`
 --
 ALTER TABLE `denda`
-  MODIFY `id_transaksi` int(4) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_transaksi` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `jenis_buku`
@@ -280,10 +333,16 @@ ALTER TABLE `jenis_buku`
   MODIFY `id_jenis_buku` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
+-- AUTO_INCREMENT for table `sk_bp`
+--
+ALTER TABLE `sk_bp`
+  MODIFY `id_laporan` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id_transaksi` int(4) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_transaksi` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
